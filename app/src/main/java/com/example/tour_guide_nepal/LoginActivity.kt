34862345867b226fe43797e25d000b9f88822c50
sjgrg22 <ikuro470@gmail.com>
2021-07-06 14:47:39 +0545

    changes in userlogin design

--- app/src/main/java/com/example/tour_guide_nepal/LoginActivity.kt
+++ app/src/main/java/com/example/tour_guide_nepal/LoginActivity.kt
@@ -8,28 +8,11 @@ import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var uname: EditText
-    private lateinit var upass: EditText
-    private lateinit var btnlogin: Button
-    private lateinit var btngoogle: Button
-    private lateinit var signup: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        uname=findViewById(R.id.uname)
-        upass=findViewById(R.id.upass)
-        btnlogin=findViewById(R.id.btnlogin)
-        btngoogle=findViewById(R.id.btngoogle)
-        signup=findViewById(R.id.signup)
-
-        btnlogin.setOnClickListener {
-            login()
-        }
     }
 
-    private fun login() {
-
-    }
 }
