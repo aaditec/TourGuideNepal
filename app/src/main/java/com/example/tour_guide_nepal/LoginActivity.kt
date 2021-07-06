302867d71f9d04530e65bad681144af40ea3c5da
aaditec <roshangautam213@gmail.com>
2021-07-06 18:46:06 +0545

    login code

--- app/src/main/java/com/example/tour_guide_nepal/LoginActivity.kt
+++ app/src/main/java/com/example/tour_guide_nepal/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tour_guide_nepal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,12 +8,59 @@ import android.widget.EditText
 import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var btnlogin: Button
+    private lateinit var register: TextView
+    private lateinit var txtname: TextView
+    private lateinit var txtpass: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnlogin = findViewById(R.id.btnlogin)
+        register = findViewById(R.id.register)
+        txtname = findViewById(R.id.txtname)
+        txtpass = findViewById(R.id.txtpass)
+
+        btnlogin.setOnClickListener {
+            login()
+
+        }
+        register.setOnClickListener {
+
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+    private fun login() {
+        if (validateLogin()) {
 
     }
 
 }
+    private fun sanitize(input : EditText) : String{
+        return input.text.toString().trim(' ')
+    }
+    private fun validateLogin(): Boolean {
+        var valid = true
+        txtname.error = null
+        txtpass.error = null
+
+        if (sanitize(txtname as EditText).isEmpty()) {
+            txtname.error = "Username can not be empty"
+            valid = false
+        }
+        if (sanitize(txtpass as EditText).isEmpty()) {
+            txtpass.error = "Password can not be empty"
+            valid = false
+        }
+
+        return valid
+    }
+
+    }
+
+
