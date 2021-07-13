6d3123a8654141d33bac78781591d7cedb5a11bf
aashishtmg <syangtanaashish7@gmail.com>
2021-07-14 01:02:17 +0545

    update

--- app/src/main/java/com/example/tour_guide_nepal/fragments/Select_cityFragment.kt
+++ app/src/main/java/com/example/tour_guide_nepal/fragments/Select_cityFragment.kt
@@ -1,14 +1,18 @@
 package com.example.tour_guide_nepal.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.example.tour_guide_nepal.R
+import com.example.tour_guide_nepal.place_details.kathmandu_detail_activity
 
 
 class Select_cityFragment : Fragment() {
+    private lateinit var kathmandu : LinearLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +25,15 @@ class Select_cityFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_city, container, false)
+        val view = inflater.inflate(R.layout.fragment_select_city, container, false)
+
+        kathmandu = view.findViewById(R.id.kathmandu)
+
+        startActivity(Intent(activity,kathmandu_detail_activity::class.java))
+
+       return view
     }
+
 
 
 }
