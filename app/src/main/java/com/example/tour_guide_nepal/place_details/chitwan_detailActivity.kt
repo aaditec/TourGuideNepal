fd31c24585f1d05e500151c667dc168564824f83
Ashishbhantana <ashish9bhantana@gmail.com>
2021-07-13 20:31:59 +0545

    chitwanplacedetail

---
+++ app/src/main/java/com/example/tour_guide_nepal/place_details/chitwan_detailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tour_guide_nepal.place_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tour_guide_nepal.R
+
+class chitwan_detailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chitwan_detail)
+    }
+}
