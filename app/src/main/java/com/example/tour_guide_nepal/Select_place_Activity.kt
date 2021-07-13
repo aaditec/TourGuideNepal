00bdc55c1972a74f4789c48dca23a0fbb7f3154a
sjgrg22 <ikuro470@gmail.com>
2021-07-13 10:10:25 +0545

    search places

---
+++ app/src/main/java/com/example/tour_guide_nepal/Select_place_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.tour_guide_nepal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Select_place_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_place)
+    }
+}
