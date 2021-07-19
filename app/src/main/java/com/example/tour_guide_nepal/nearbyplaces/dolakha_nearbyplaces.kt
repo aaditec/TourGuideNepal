4a913fdcefc7234bea85884598d1d9892cb5cd2c
sjgrg22 <ikuro470@gmail.com>
2021-07-19 13:33:17 +0545

    near by places

---
+++ app/src/main/java/com/example/tour_guide_nepal/nearbyplaces/dolakha_nearbyplaces.kt
@@ -0,0 +1,12 @@
+package com.example.tour_guide_nepal.nearbyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tour_guide_nepal.R
+
+class dolakha_nearbyplaces : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dolakha_nearbyplaces)
+    }
+}
---
+++ app/src/main/java/com/example/tour_guide_nepal/nearbyplaces/pokhara_nearbyplaces.kt
@@ -0,0 +1,12 @@
+package com.example.tour_guide_nepal.nearbyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tour_guide_nepal.R
+
+class pokhara_nearbyplaces : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokhara_nearbyplaces)
+    }
+}
