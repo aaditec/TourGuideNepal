fd49e2102f45f3cc6c79ab3a75bd9b8f2e5cc031
aashishtmg <syangtanaashish7@gmail.com>
2021-07-19 12:05:43 +0545

    adding home

--- app/src/main/java/com/example/tour_guide_nepal/fragments/Select_cityFragment.kt
+++ app/src/main/java/com/example/tour_guide_nepal/fragments/Select_cityFragment.kt
@@ -1,28 +1,20 @@
 package com.example.tour_guide_nepal.fragments
 
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.Button
+
 import com.example.tour_guide_nepal.R
-import com.example.tour_guide_nepal.place_details.*
+import com.example.tour_guide_nepal.place_details.chitwan_detail_activity
 
 
 class Select_cityFragment : Fragment() {
-    private lateinit var kathmandu : LinearLayout
-    private lateinit var chitwan : LinearLayout
-    private lateinit var dolakha : LinearLayout
-    private lateinit var gorkha : LinearLayout
-    private lateinit var janakpur : LinearLayout
-    private lateinit var lumbini : LinearLayout
-    private lateinit var mustang : LinearLayout
-    private lateinit var pokhara : LinearLayout
-    private lateinit var solukhumbu : LinearLayout
-    private lateinit var tanahun : LinearLayout
-
+  private lateinit var selectplace: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,53 +25,16 @@ class Select_cityFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_select_city, container, false)
+        selectplace = view.findViewById(R.id.selectplace)
+        selectplace.setOnClickListener {
+            startActivity(Intent(activity, Selectplaces::class.java))
 
-        kathmandu = view.findViewById(R.id.kathmandu)
-        chitwan = view.findViewById(R.id.chitwan)
-        janakpur = view.findViewById(R.id.janakpur)
-        dolakha = view.findViewById(R.id.dolakha)
-        pokhara = view.findViewById(R.id.pokhara)
-        solukhumbu = view.findViewById(R.id.solukhumbu)
-        lumbini = view.findViewById(R.id.lumbini)
-        mustang = view.findViewById(R.id.mustang)
-        tanahun = view.findViewById(R.id.tanahun)
-        gorkha = view.findViewById(R.id.gorkha)
+        }
 
-    kathmandu.setOnClickListener {
-    startActivity(Intent(activity, kathmandu_detail_activity::class.java))
+        return view
+
     }
-        dolakha.setOnClickListener {
-            startActivity(Intent(activity, dolakha_detail_activity::class.java))
-        }
-        chitwan.setOnClickListener {
-            startActivity(Intent(activity, chitwan_detail_activity::class.java))
-        }
-        gorkha.setOnClickListener {
-            startActivity(Intent(activity, gorkha_detail_activity::class.java))
-        }
-        janakpur.setOnClickListener {
-            startActivity(Intent(activity, janakpur_detail_activity::class.java))
-        }
-        lumbini.setOnClickListener {
-            startActivity(Intent(activity, lumbini_detail_activity::class.java))
-        }
-        mustang.setOnClickListener {
-            startActivity(Intent(activity, mustang_detail_activity::class.java))
-        }
-        pokhara.setOnClickListener {
-            startActivity(Intent(activity, pokhara_detail_activity::class.java))
-        }
-        solukhumbu.setOnClickListener {
-            startActivity(Intent(activity, solukhumbu_detail_activity::class.java))
-        }
-        tanahun.setOnClickListener {
-            startActivity(Intent(activity, tanahun_detail_activity::class.java))
-        }
-       return view
-    }
-
-
-
 }
