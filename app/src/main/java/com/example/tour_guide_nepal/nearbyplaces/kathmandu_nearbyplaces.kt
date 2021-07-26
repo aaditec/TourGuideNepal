64537b4c3420428b14c1e234ba10258863de702f
aaditec <roshangautam213@gmail.com>
2021-07-26 12:58:49 +0545

    hotel booking intent pass

--- app/src/main/java/com/example/tour_guide_nepal/nearbyplaces/kathmandu_nearbyplaces.kt
+++ app/src/main/java/com/example/tour_guide_nepal/nearbyplaces/kathmandu_nearbyplaces.kt
@@ -2,6 +2,7 @@ package com.example.tour_guide_nepal.nearbyplaces
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tour_guide_nepal.Hotelbooking_Activity
 import com.example.tour_guide_nepal.R
 import com.example.tour_guide_nepal.service.model.NearByPlaceModel
 import com.example.tour_guide_nepal.view.adapter.NearByPlacesAdapter
@@ -48,7 +50,9 @@ class kathmandu_nearbyplaces : AppCompatActivity() {
 
         Atm.setOnClickListener { getPlaces("atm") }
 
-        Hotel.setOnClickListener { getPlaces("Hotel") }
+        Hotel.setOnClickListener { getPlaces("Hotel")
+            val intent = Intent(this, Hotelbooking_Activity::class.java)
+            startActivity(intent)}
         Policestation.setOnClickListener { getPlaces("Police Station")
 
 
