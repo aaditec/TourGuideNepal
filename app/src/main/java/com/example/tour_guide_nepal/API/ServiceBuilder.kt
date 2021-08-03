ad10b27a0f66427e73902247fc14122290c5fbb5
sjgrg22 <ikuro470@gmail.com>
2021-08-03 21:17:34 +0545

    changes

--- app/src/main/java/com/example/tour_guide_nepal/API/ServiceBuilder.kt
+++ app/src/main/java/com/example/tour_guide_nepal/API/ServiceBuilder.kt
@@ -5,8 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val BASE_URL = "http://10.0.2.2:90/"
- //private const val BASE_URL = "http://192.168.137.1:90/"
+//    private const val BASE_URL = "http://10.0.2.2:90/"
+ private const val BASE_URL = "http://192.168.0.101:90/"
 
 
 
