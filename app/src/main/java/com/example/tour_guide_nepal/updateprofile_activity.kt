7b8f2fb2ca151b741231de0a7a2ac422568acb84
sjgrg22 <ikuro470@gmail.com>
2021-08-24 11:56:18 +0545

    sprint8

---
+++ app/src/main/java/com/example/tour_guide_nepal/updateprofile_activity.kt
@@ -0,0 +1,11 @@
+package com.example.tour_guide_nepal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class updateprofile_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_updateprofile)
+    }
+}
