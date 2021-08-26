981ca41188c2a181eb952c60b100f9ff8140363a
sjgrg22 <ikuro470@gmail.com>
2021-08-26 10:23:40 +0545

    terms and services

---
+++ app/src/main/java/com/example/tour_guide_nepal/termsandservices/back_terms_and_services.kt
@@ -0,0 +1,11 @@
+package com.example.tour_guide_nepal.termsandservices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class back_terms_and_services : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back_terms_and_services)
+    }
+}
---
+++ app/src/main/java/com/example/tour_guide_nepal/termsandservices/front_terms_and_services.kt
@@ -0,0 +1,11 @@
+package com.example.tour_guide_nepal.termsandservices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class front_terms_and_services : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_terms_and_services)
+    }
+}
