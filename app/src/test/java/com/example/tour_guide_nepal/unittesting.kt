d35eadbb81941176a3baca0079feee5c8f79c82d
aaditec <roshangautam213@gmail.com>
2021-09-06 11:40:53 +0545

    error

--- app/src/test/java/com/example/tour_guide_nepal/unittesting.kt
+++ app/src/test/java/com/example/tour_guide_nepal/unittesting.kt
@@ -1,8 +1,13 @@
 package com.example.tour_guide_nepal
 
 
+import com.example.tour_guide_nepal.API.ServiceBuilder
+import com.example.tour_guide_nepal.ENTITY.HotelBookDetails
 import com.example.tour_guide_nepal.ENTITY.User
+import com.example.tour_guide_nepal.ENTITY.VehicleRentEntity
+import com.example.tour_guide_nepal.Repository.HotelBookRepository
 import com.example.tour_guide_nepal.Repository.UserRepository
+import com.example.tour_guide_nepal.Repository.VehicleRentRepository
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
@@ -10,6 +15,8 @@ import org.junit.Test
 class unittesting {
 
     private lateinit var userRepository: UserRepository
+    private lateinit var hotelbookrepository: HotelBookRepository
+    private lateinit var vehiclerentrepository: VehicleRentRepository
     //=======login testing======
     @Test
     fun checklogin() = runBlocking {
@@ -38,5 +45,30 @@ class unittesting {
         val actualResult = response.success
         Assert.assertEquals(expectedResult, actualResult)
     }
+    @Test
+    fun addhotelbook() = runBlocking {
+        userRepository = UserRepository()
+        hotelbookrepository = HotelBookRepository()
 
+        val hotel =
+            HotelBookDetails(fullname = "aaditya chhrsg",email = "rohan@gmail.com", phone = "8888888888", hotelname = "jacob", roomtype = "single", datefrom = "9/6/2021", dateto = "9/8/2021",numberofpeople = "4",comments = "hello")
+
+        ServiceBuilder.token ="Bearer " + userRepository.loginUser("roshan11@gmail.com","11111111").token
+        val expectedResult = true
+        val actualResult = hotelbookrepository.bookHotel(hotel).success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    fun addvehiclebook() = runBlocking {
+        userRepository = UserRepository()
+        vehiclerentrepository = VehicleRentRepository()
+
+        val Vehicle =
+            VehicleRentEntity(fullname = "aaditya chhrsg",email = "rohan@gmail.com", phone = "8888888888",   tripstartdate = "9/6/2021", tripenddate = "9/8/2021",numberofpeople = "4",traveldetail = "hello")
+
+        ServiceBuilder.token ="Bearer " + userRepository.loginUser("roshan11@gmail.com","11111111").token
+        val expectedResult = true
+        val actualResult = vehiclerentrepository.rentvehicle(Vehicle).success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
 }
